4616db981d3143f2bab4140023eab254be416008
Luis Henriques <luismatahenriques@gmail.com>
2023-10-16 11:18:38 +0100

    initial refactor

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Square = props => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+        <button className="square" onClick={props.onClick}>
+            {/* TODO */}
+        </button>
+    );
+}
+
+const Board = props => {
+
+    const renderSquare = (i) => {
+        return <Square key={`square-${i}`} onClick={handleClick} />;
+    }
+
+    const renderLine = () => {
+        return (
+            <div className="board-row">
+                {squares}
+            </div>
+        )
+    }
+
+    const handleClick = () => {
+        console.log("clicked!")
+    }
+
+    const squares = new Array(9).fill(null).map((element, index) => {
+        return renderSquare(index);
+    });
+
+    return (
+        <div>
+            {renderLine()} 
+            {renderLine()} 
+            {renderLine()}
+            {renderLine()} 
+            {renderLine()} 
+            {renderLine()}  
+            {renderLine()} 
+            {renderLine()} 
+            {renderLine()} 
+        </div>
+    );
+}
+
+const Game = () => {
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+        </div>
+    );
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
